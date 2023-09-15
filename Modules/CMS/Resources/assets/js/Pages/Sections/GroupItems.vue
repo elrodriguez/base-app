2749c1c7c9c75d676f19a69fd025f723b1ad9aaf
elrodriguez <elrodriguez2423@gmail.com>
2023-09-15 17:33:43 -0500

    e

--- Modules/CMS/Resources/assets/js/Pages/Sections/GroupItems.vue
+++ Modules/CMS/Resources/assets/js/Pages/Sections/GroupItems.vue
@@ -1,6 +1,6 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
-import GroupItemsForm from './items/GroupItemsForm.vue';
+import GroupItemsForm from './Partials/GroupItemsForm.vue';
 import { Link } from '@inertiajs/vue3';
 
 const props = defineProps({
