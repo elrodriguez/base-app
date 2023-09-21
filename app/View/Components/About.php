fe74ac3f2b6c6b65b24cff5b4a4e3571f7e76f3c
Grandejosh <60234854+Grandejosh@users.noreply.github.com>
2023-09-20 22:46:43 -0500

    ABOUT-listo
    sd

---
+++ app/View/Components/About.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+use Modules\CMS\Entities\CmsSectionItem;
+
+class About extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct()
+    {
+        $this->data = CmsSectionItem::with('item.items')->where('section_id', 4)->get();
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.about', [
+            'data' => $this->data
+        ]);
+    }
+}
