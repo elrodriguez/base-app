3373862a83e437e97c1280b5c96b45a29faef218
elrodriguez <elrodriguez2423@gmail.com>
2023-09-23 20:35:40 -0500

    s

--- Modules/CMS/Resources/assets/js/Pages/Sections/Items/List.vue
+++ Modules/CMS/Resources/assets/js/Pages/Sections/Items/List.vue
@@ -270,6 +270,9 @@
                                             </template>
                                             <input style="width: 70px;" class="mt-1 mr-2" type="number" v-model="item.si_position">
                                             <button @click="destroyItem(item.si_id)" type="button" class="px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-1">Eliminar Item</button>
+                                            <template v-if="item.type_id == 5">
+                                                <span>{{ item.description }}</span>
+                                            </template>
                                         </li>
                                     </ul>
                                 </div>
