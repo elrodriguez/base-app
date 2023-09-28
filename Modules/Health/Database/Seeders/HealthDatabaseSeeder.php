f6bc7b86f9089f6d0b77efbb26fbf6d7276a3b80
Grandejosh <60234854+Grandejosh@users.noreply.github.com>
2023-09-27 19:44:53 -0500

    creado modulo SALUD Health

---
+++ Modules/Health/Database/Seeders/HealthDatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\Health\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class HealthDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        // $this->call("OthersTableSeeder");
+    }
+}
---
+++ Modules/Health/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('health::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('health.name') !!}
+    </p>
+@endsection
