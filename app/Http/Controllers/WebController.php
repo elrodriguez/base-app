497b81df18bad4f9ecb4ff8cb0ac8990ccc7da11
Grandejosh <60234854+Grandejosh@users.noreply.github.com>
2023-11-03 00:44:02 -0500

    test imagen

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -33,10 +33,13 @@ class WebController extends Controller
         ]);
     }
 
-    public function testimage($content){
+    public function testimage($content, $fecha=null){
 
+        if($fecha==null){
+            echo "Agrega un Slash '/' y agrega la fecha ejemplo 'test-image/Miguel de Cervantes Saavedra/23-01-2021'";
+        }else{
         // create Image from file
-$img = Image::make('http://base-app.test/storage/uploads/onlineshop/items/20231031043434.jpg');
+$img = Image::make('https://aprendiendoconsira.com/wp-content/uploads/2022/06/5-2000x1000.png');
 
 // write text
 //$img->text('The quick brown fox jumps over the lazy dog.');
@@ -44,17 +47,48 @@ $img = Image::make('http://base-app.test/storage/uploads/onlineshop/items/202310
 // write text at position
 //$img->text('The quick brown fox jumps over the lazy dog.', 120, 100);
 
-
-
-
-$img->text($content, 300, 500, function($font) {
+$img->text($content, 1070, 496, function($font) {
     $font->file('fonts/OLDENGL.TTF');
-    $font->size(46);
-    $font->color('#0d06e3');
+    $font->size(72);
+    $font->color('#0d0603');
     $font->align('center');
     $font->valign('top');
     $font->angle(0);
 });
+
+$img->text("Entregado el: ".$fecha, 120, 15, function($font) {
+    $font->file('fonts/OLDENGL.TTF');
+    $font->size(40);
+    $font->color('#0d0603');
+    $font->align('left');
+    $font->valign('top');
+    $font->angle(0);
+});
+
+$img->text("Pragot Especialistas en Especialización y mejora continua.", 1840, 15, function($font) {
+    $font->file('fonts/OLDENGL.TTF');
+    $font->size(40);
+    $font->color('#0d0603');
+    $font->align('right');
+    $font->valign('top');
+    $font->angle(0);
+});
+
+$qr = Image::make('https://borealtech.com/wp-content/uploads/2018/10/codigo-qr-1024x1024-1.jpg');
+$qr->fit(200, 200);
+$img->insert($qr, 'bottom-left', 30, 30);
+
+
+// Ejemplo de Redimensionar la imagen manteniendo la proporción para avatares y similares
+// Establecer el ancho máximo y la altura máxima deseados
+$maxWidth = 1550;
+$maxHeight = 1550;
+$img->resize($maxWidth, $maxHeight, function ($constraint) {
+    $constraint->aspectRatio();
+    $constraint->upsize();
+});
+
+
 
 // Obtener el contenido binario de la imagen
 $imageContent = $img->encode('png');
@@ -67,5 +101,6 @@ $response->header('Content-Type', 'image/png');
 
 // Retornar la respuesta
 return $response;
+        }
     }
 }
