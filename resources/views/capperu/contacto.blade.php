7c0a3fd432c4b2d0d721ee9f6bebe31a7efe09ce
Grandejosh <60234854+Grandejosh@users.noreply.github.com>
2023-11-10 01:13:41 -0500

    _contactos  subscribers
    _

--- resources/views/capperu/contacto.blade.php
+++ resources/views/capperu/contacto.blade.php
@@ -37,22 +37,23 @@
             <div class="row justify-content-center">
                 <div class="col-md-8">
                     <div class="contact-inner">
-                        <form class="contact-form">
+                        <form id="pageContactForm" class="contact-form" method="post" action="{{ route('apisubscriber') }}">
+                            @csrf
                             <div class="row">
                                 <div class="col-md-12 single-input-wrap">
-                                    <input type="text" placeholder="Nombres completos">
+                                    <input type="text" placeholder="Nombres completos" name="full_name">
                                 </div>
                                 <div class="col-md-5 single-input-wrap">
-                                    <input type="text" placeholder="Teléfono">
+                                    <input type="text" placeholder="Teléfono" name="phone">
                                 </div>
                                 <div class="col-md-7 single-input-wrap">
-                                    <input type="text" placeholder="E-mail">
+                                    <input type="email" placeholder="E-mail" name="email">
                                 </div>
                                 <div class="col-md-12 single-input-wrap">
-                                    <textarea rows="5" placeholder="Escribe tu mensaje"></textarea>
+                                    <textarea rows="5" placeholder="Escribe tu mensaje" name="message"></textarea>
                                 </div>    
                             </div>                                
-                            <button type="submit" class="btn btn-base w-100">Enviar Ahora</button>
+                            <button id="submitPageContactButton" type="submit" class="btn btn-base w-100">Enviar Ahora</button>
                         </form>
                     </div>
                 </div>
@@ -103,7 +104,64 @@
         </div>
     </section>
     <!-- zoom courses Area End -->
+    <script>
+        let form = document.getElementById('pageContactForm');
+        form.addEventListener('submit', function(e) {
+            e.preventDefault();
 
+            var formulario = document.getElementById('pageContactForm');
+            var formData = new FormData(formulario);
+
+            // Deshabilitar el botón
+            var submitButton = document.getElementById('submitPageContactButton');
+            submitButton.disabled = true;
+            submitButton.style.opacity = 0.25;
+
+            // Crear una nueva solicitud XMLHttpRequest
+            var xhr = new XMLHttpRequest();
+
+            // Configurar la solicitud POST al servidor
+            xhr.open('POST', "{{ route('apisubscriber') }}", true);
+
+            // Configurar la función de callback para manejar la respuesta
+            xhr.onload = function() {
+                // Habilitar nuevamente el botón
+                submitButton.disabled = false;
+                submitButton.style.opacity = 1;
+                if (xhr.status === 200) {
+                    var response = JSON.parse(xhr.responseText);
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Enhorabuena',
+                        text: response.message,
+                        customClass: {
+                            container: 'sweet-modal-zindex' // Clase personalizada para controlar el z-index
+                        }
+                    });
+                    formulario.reset();
+                } else if (xhr.status === 422) {
+                    var errorResponse = JSON.parse(xhr.responseText);
+                    // Maneja los errores de validación aquí, por ejemplo, mostrando los mensajes de error en algún lugar de tu página.
+                    var errorMessages = errorResponse.errors;
+                    var errorMessageContainer = document.getElementById('messagePageContact');
+                    errorMessageContainer.innerHTML = 'Errores de validación:<br>';
+                    for (var field in errorMessages) {
+                        if (errorMessages.hasOwnProperty(field)) {
+                            errorMessageContainer.innerHTML += field + ': ' + errorMessages[field].join(', ') +
+                                '<br>';
+                        }
+                    }
+                } else {
+                    console.error('Error en la solicitud: ' + xhr.status);
+                }
+
+
+            };
+
+            // Enviar la solicitud al servidor
+            xhr.send(formData);
+        });
+    </script>
     
     <!-- Más Populares Area Start-->
     <x-capperu.programas-populares-area></x-capperu.programas-populares-area>
